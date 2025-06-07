2f1868acbeb6de8146e56872538efb54b5041121
Isaac Picov <104283077+IsaacPicov@users.noreply.github.com>
2025-06-07 04:04:40 -0400

    Ablations

--- sandbox/run_all.sh
+++ sandbox/run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #MLP Ablations 
-echo "MLP 3 layers"
-python3 main.py train --exp_name GAT_MLP_3 --num_MLP_layers 3
+# echo "MLP 3 layers"
+# python3 main.py train --exp_name GAT_MLP_3 --num_MLP_layers 3
 
 echo "MLP 5 layers"
-python3 main.py train --exp_name GAT_MLP_3 --num_MLP_layers 5
+python3 main.py train --exp_name GAT_MLP_5 --num_MLP_layers 5 --batch_size 64
 
 echo "MLP 1 layer"
-python3 main.py train --exp_name GAT_MLP_3 --num_MLP_layers 1
+python3 main.py train --exp_name GAT_MLP_1 --num_MLP_layers 1 --batch_size 64
